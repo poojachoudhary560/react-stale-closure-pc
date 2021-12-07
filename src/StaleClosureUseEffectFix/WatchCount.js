085be9d830f9ff11d6f797e0ba5929736b9817b9
poojachoudhary560 <poojachoudhary560@gmail.com>
2021-12-08 02:35:23 +0530

    1 file modified

--- src/StaleClosureUseEffectFix/WatchCount.js
+++ src/StaleClosureUseEffectFix/WatchCount.js
@@ -4,12 +4,12 @@ const WatchCount = (props) => {
   const [count, setCount] = useState(0);
   useEffect(
     function () {
-      const id = setInterval(function log() {
+      /* const id = setInterval(function log() {
         console.log(`Value of count fix is ${count}`);
       }, 3000);
       return function () {
         clearInterval(id);
-      };
+      }; */
     },
     [count]
   );
