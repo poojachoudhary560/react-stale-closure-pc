182695747d2e93f56144a41018bb75fc7610a59c
poojachoudhary560 <poojachoudhary560@gmail.com>
2021-12-08 02:07:27 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import StaleClosure from './StaleClosure';
+import StaleClosureFix from './StaleClosureFix';
 import './style.css';
 
 export default function App() {
@@ -7,6 +8,8 @@ export default function App() {
     <div>
       <h4>Stale Closure JS </h4>
       <StaleClosure />
+      <h4>Stale Closure JS Fix </h4>
+      <StaleClosureFix />
     </div>
   );
 }
